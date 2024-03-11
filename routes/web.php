efbb711be6146454fe0ebc573298cea997a4ca37
saricomel <rahmahksp80@gmail.com>
2024-03-11 14:05:14 +0700

    berhasil buat manejemen categori dan manajemen customer lupa commmit di manajemen categori sudah terlanjur

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Models\Customer;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +25,6 @@ Route::get('/', function () {
         "title"=>"Dashboard"
     ]);
 });
+Route::resource('kategori',CategoryController::class)
+->except('show','destroy','create');
+Route::resource('pelanggan',CustomerController::class)->except('destroy');
